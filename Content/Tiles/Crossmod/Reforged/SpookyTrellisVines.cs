edd7e4d6501815e2e97a73e8f909fbb537c951fc
DylanDoe211 <dylandutes@gmail.com>
2026-09-10 10:32:53 -0400

    Other trellis vines

--- Content/Tiles/Crossmod/Reforged/SpookyTrellisVines.cs
+++ Content/Tiles/Crossmod/Reforged/SpookyTrellisVines.cs
@@ -1,13 +1,18 @@
-﻿using Spooky.Content.Tiles.SpookyBiome;
+﻿using Terraria.ModLoader;
 using System;
-using Terraria.ModLoader;
+
+using Spooky.Content.Tiles.Cemetery;
+using Spooky.Content.Tiles.Minibiomes.Vegetable;
+using Spooky.Content.Tiles.SpiderCave;
+using Spooky.Content.Tiles.SpookyBiome;
+using Spooky.Content.Tiles.SpookyHell;
 
 namespace Spooky.Content.Tiles.Crossmod.Reforged;
 
 /// <summary>
 /// Loader class for the crossmod trellis vine tiles.
 /// </summary>
-internal class TrellisVines : ILoadable
+internal class SpookyTrellisVines : ILoadable
 {
 	void ILoadable.Load(Mod mod)
 	{
@@ -23,8 +28,14 @@ internal class TrellisVines : ILoadable
 		reforged.Call("TrellisVine", ModContent.GetInstance<Spooky>(), (Func<(int, int)[]>)GetStyles, "SpookyTrellisVines", path);
 	}
 
-	public static (int, int)[] GetStyles() => [(ModContent.ItemType<SpookySeedsGreen>(), 0), (ModContent.ItemType<SpookySeedsOrange>(), 1),
-		(ModContent.ItemType<MushroomMossSeeds>(), 2) ];
+	public static (int, int)[] GetStyles() => [
+	(ModContent.ItemType<SpookySeedsGreen>(), 0), 
+	(ModContent.ItemType<SpookySeedsOrange>(), 1),
+	(ModContent.ItemType<MushroomMossSeeds>(), 2),
+	(ModContent.ItemType<CemeteryGrassSeeds>(), 3),
+	(ModContent.ItemType<JungleSoilGrassSeeds>(), 4),
+	(ModContent.ItemType<DampGrassSeeds>(), 5),
+	(ModContent.ItemType<SpookyMushGrassSeeds>(), 6) ];
 
 	void ILoadable.Unload() { }
 }
